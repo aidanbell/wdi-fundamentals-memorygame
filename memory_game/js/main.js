59e0724244a7261588f73cb69df5079aca325d90
unknown <aidanbell0182@gmail.com>
2019-08-21 13:12:22 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,43 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  },
+];
+
 var cardsInPlay = [];
 
 function checkForMatch() {
   if (cardsInPlay.length === 2) {
     if(cardsInPlay[0] === cardsInPlay[1]) {
-      console.log("You found a match!");
+      alert("You found a match!");
     } else {
-      console.log("Sorry, try again");
+      alert("Sorry, try again");
     }
   }
 }
 
 function flipCard(cardId) {
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId])
+  console.log("User flipped " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank)
+  console.log(cards[cardId].cardImage)
+  console.log(cards[cardId].suit)
   checkForMatch();
 }
 
