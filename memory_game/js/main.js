0320957f672c8b4245e8dec2c68928d5d20337dc
unknown <aidanbell0182@gmail.com>
2019-08-21 12:26:02 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,21 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-  if(cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again");
+function checkForMatch() {
+  if (cardsInPlay.length === 2) {
+    if(cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+    } else {
+      console.log("Sorry, try again");
+    }
   }
 }
+
+function flipCard(cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId])
+  checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
